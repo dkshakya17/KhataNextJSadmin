72a0fbdff37a9728463a296ee2f4296ccd70b747
Deepak kumar <deepak.kumar@cashfree.com>
2020-12-22 21:41:41 +0530

    change theme

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -17,10 +17,7 @@ import SEO, { SEOProps } from 'components/SEO';
 const getDefaultTheme = (): DefaultTheme['name'] => {
   if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
     return localStorage.getItem('theme') as DefaultTheme['name'];
-  } else {
-    const hours = new Date().getHours();
-    return hours > 6 && hours < 19 ? 'default' : 'dark';
-  }
+  } 
 };
 
 const LayoutPage: React.FC<SEOProps> = ({ children, ...rest }) => {
