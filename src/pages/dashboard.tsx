c9076ab12391c7f34622f7214cfb87e24751828b
Aashay Chaturvedi <chaturvedi.aashay29@gmail.com>
2020-12-27 17:25:21 +0530

    added Get APIs

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,93 +1,93 @@
-
-import { List, ListItem } from '@paljs/ui/List';
-import { Card, CardBody, CardHeader, CardFooter  } from '@paljs/ui/Card';
-import User from '@paljs/ui/User';
-import 'reactjs-popup/dist/index.css';
-import { Actions, ActionType } from '@paljs/ui/Actions';
-import { Search } from '@paljs/ui/Search';
-import styled from 'styled-components';
-import Row from '@paljs/ui/Row';
-import Col from '@paljs/ui/Col';
-import { Button } from '@paljs/ui/Button';
-import React, { useState }  from 'react';
-import Popup from 'reactjs-popup';
-import Layout from 'Layouts';
-import { InputGroup } from '@paljs/ui/Input';
-import Link from 'next/link';
+import { List, ListItem } from "@paljs/ui/List";
+import { Card, CardBody, CardHeader, CardFooter } from "@paljs/ui/Card";
+import User from "@paljs/ui/User";
+import "reactjs-popup/dist/index.css";
+import { Actions, ActionType } from "@paljs/ui/Actions";
+import { Search } from "@paljs/ui/Search";
+import styled from "styled-components";
+import Row from "@paljs/ui/Row";
+import Col from "@paljs/ui/Col";
+import { Button } from "@paljs/ui/Button";
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
+import Layout from "Layouts";
+import { InputGroup } from "@paljs/ui/Input";
+import Link from "next/link";
 
 const TableStyle = styled.div`
-display: -webkit-box;
-display: -webkit-flex;
-display: -ms-flexbox;
-display: flex;
--webkit-flex-direction: column;
--ms-flex-direction: column;
-flex-direction: column;
--webkit-flex: 1 1 auto;
--ms-flex: 1 1 auto;
-flex: 1 1 auto;
-overflow: auto;
-width:100%;
-.trasactions_table {
-  width:100%;
-}
-.trasactions_table td,.trasactions_table th{
- padding:15px;
- border-bottom: 1px solid #ccc;
-}
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-flex: 1 1 auto;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  overflow: auto;
+  width: 100%;
+  .trasactions_table {
+    width: 100%;
+  }
+  .trasactions_table td,
+  .trasactions_table th {
+    padding: 15px;
+    border-bottom: 1px solid #ccc;
+  }
 `;
 const SearchCss = styled.div`
   float: right;
 `;
 const PartnerHead = styled.div`
-header{
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: center;
-  padding:10px 20px;
-}
+  header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    padding: 10px 20px;
+  }
 `;
 
 const PartnerCss = styled.div`
-display: flex;
-justify-content: space-between;
-width: 100%;
-cursor:pointer;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  cursor: pointer;
 `;
-const Home = () => {
-  const [, setValue] = useState('');
+const Home = ({ partners }) => {
+  const [, setValue] = useState("");
   const submitHandle = (sentValue: string) => setValue(sentValue);
   const userList = [
-  { name: 'Carla Espinosa', amount: '2000' },
-    { name: 'Bob Kelso', amount: '4000' },
-    { name: 'Janitor',amount: '5000' },
-    { name: 'Perry Cox', amount: '20000' },
-    { name: 'Ben Sullivan', amount: '21000' },
+    { name: "Carla Espinosa", amount: "2000" },
+    { name: "Bob Kelso", amount: "4000" },
+    { name: "Janitor", amount: "5000" },
+    { name: "Perry Cox", amount: "20000" },
+    { name: "Ben Sullivan", amount: "21000" },
   ];
   const items: ActionType[] = [
     {
-      icon: 'refresh-outline',
+      icon: "refresh-outline",
       disabled: false,
-    }
+    },
   ];
   const transcList = [
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' },
-    { name: 'Deepak', item: 'Lemons', quantity: '5Kg',Price:'200' }
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
+    { name: "Deepak", item: "Lemons", quantity: "5Kg", Price: "200" },
   ];
   return (
     <Layout title="Home">
@@ -115,58 +115,80 @@ const Home = () => {
         </Col>
       </Row>
       <Row>
-      <Col breakPoint={{ xs: 12, md: 6 }}>
+        <Col breakPoint={{ xs: 12, md: 6 }}>
           <Card size="Small" accent="Primary">
             <PartnerHead>
-            <header>
-                
-                  <Popup
-                    trigger={ <Button appearance="outline" status="Primary" size="Small">
-                    + Add 
-                  </Button>}
-                    modal
-                    nested
-                  >
-                {close => (
-                  <div className="modal popup_modal">
-                    <div className="modal_head">
-                    
-                    <div className="popup_heading"> Add Partner </div>
-                    <Button appearance="outline" status="Primary" onClick={close} size="Small" className="close">
-                    &times;
+              <header>
+                <Popup
+                  trigger={
+                    <Button appearance="outline" status="Primary" size="Small">
+                      + Add
                     </Button>
+                  }
+                  modal
+                  nested
+                >
+                  {(close) => (
+                    <div className="modal popup_modal">
+                      <div className="modal_head">
+                        <div className="popup_heading"> Add Partner </div>
+                        <Button
+                          appearance="outline"
+                          status="Primary"
+                          onClick={close}
+                          size="Small"
+                          className="close"
+                        >
+                          &times;
+                        </Button>
+                      </div>
+                      <div className="content">
+                        <form action="submit" className="add_form">
+                          <InputGroup fullWidth>
+                            <input type="text" placeholder="Partner Name" />
+                          </InputGroup>
+                          <InputGroup fullWidth>
+                            <input type="number" placeholder="Phone Number" />
+                          </InputGroup>
+                        </form>
+                      </div>
+                      <div className="actions button_sbmit">
+                        <Button
+                          appearance="outline"
+                          status="Primary"
+                          onClick={close}
+                          size="Small"
+                          className="close"
+                        >
+                          Submit
+                        </Button>
+                      </div>
                     </div>
-                    <div className="content">
-                     <form action="submit" className="add_form">
-                     <InputGroup fullWidth>
-                      <input type="text" placeholder="Partner Name" />
-                    </InputGroup>
-                    <InputGroup fullWidth>
-                      <input type="number" placeholder="Phone Number" />
-                    </InputGroup>
-                     </form>
-                    </div>
-                    <div className="actions button_sbmit">
-                      <Button appearance="outline" status="Primary" onClick={close} size="Small" className="close">Submit</Button>
-                    </div>
-                  </div>
-                )}
-            </Popup>
-              <p><b>Partners</b></p>
-              <SearchCss>
-                <Search submit={(v) => submitHandle(v)} type={'column-curtain'} placeholder="Search..." hint="Hit Enter to search" className="search_icon" />
-              </SearchCss> 
-            </header>
+                  )}
+                </Popup>
+                <p>
+                  <b>Partners</b>
+                </p>
+                <SearchCss>
+                  <Search
+                    submit={(v) => submitHandle(v)}
+                    type={"column-curtain"}
+                    placeholder="Search..."
+                    hint="Hit Enter to search"
+                    className="search_icon"
+                  />
+                </SearchCss>
+              </header>
             </PartnerHead>
             <List>
-              {userList.map((user, index) => (
+              {partners.map((user, index) => (
                 <Link href="/partners">
-                <ListItem key={index} className="partnertile">
-                  <PartnerCss>
-                  <User name={user.name} />
-                  <div className="usramount">(Rs {user.amount})</div>
-                  </PartnerCss> 
-                </ListItem>
+                  <ListItem key={index} className="partnertile">
+                    <PartnerCss>
+                      <User name={user.name} />
+                      <div className="usramount">(Rs {user.creditDue})</div>
+                    </PartnerCss>
+                  </ListItem>
                 </Link>
               ))}
             </List>
@@ -174,34 +196,66 @@ const Home = () => {
         </Col>
         <Col breakPoint={{ xs: 12, md: 6 }}>
           <Card size="Small" accent="Primary">
-            <header>Recent Trasactions  
-            <SearchCss>
-            <Actions Link={Link} size="Small" actions={items} className="refresh_btn"/>
-            </SearchCss>
+            <header>
+              Recent Trasactions
+              <SearchCss>
+                <Actions
+                  Link={Link}
+                  size="Small"
+                  actions={items}
+                  className="refresh_btn"
+                />
+              </SearchCss>
             </header>
-           <TableStyle>
-            <table className="trasactions_table">
-              <tr>
-                <th>Name</th>
-                <th>Item</th>
-                <th>Quantity</th>
-                <th>Price</th>
-              </tr>
-              {transcList.map((transc, index) => (
-              <tr key={index}>
-                <td>{transc.name}</td>
-                <td>{transc.item}</td>
-                <td>{transc.quantity}</td>
-                <td>{transc.Price}</td>
-              </tr>
+            <TableStyle>
+              <table className="trasactions_table">
+                <tr>
+                  <th>Name</th>
+                  <th>Item</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+                {partners.map((transc, index) => (
+                  <tr key={index}>
+                    <td>{transc.name}</td>
+                    <td>{transc.mobileNo}</td>
+                    <td>{transc.totalSale}</td>
+                    <td>{transc.totalCashReceived}</td>
+                  </tr>
                 ))}
               </table>
-              </TableStyle>
+            </TableStyle>
           </Card>
-        </Col> 
+        </Col>
       </Row>
     </Layout>
-
   );
 };
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  // const res = await fetch('http://3.7.70.121:8080/distributor/list/customer?distributorId=1')
+  // const response = await res.json()
+
+  const partnersRes = await fetch(
+    "http://3.7.70.121:8080/distributor/list/customer?distributorId=1"
+  );
+  const partners = await partnersRes.json();
+
+  // console.log("partners = ", partners);
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      // response,
+      partners,
+    },
+  };
+}
+
 export default Home;
--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,187 +1,193 @@
+import { List, ListItem } from "@paljs/ui/List";
+import { Card, CardBody, CardHeader, CardFooter } from "@paljs/ui/Card";
+import User from "@paljs/ui/User";
+import { Search } from "@paljs/ui/Search";
+import styled from "styled-components";
+import Row from "@paljs/ui/Row";
+import Col from "@paljs/ui/Col";
+import { Button } from "@paljs/ui/Button";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
-import { List, ListItem } from '@paljs/ui/List';
-import { Card, CardBody, CardHeader, CardFooter  } from '@paljs/ui/Card';
-import User from '@paljs/ui/User';
-import { Search } from '@paljs/ui/Search';
-import styled from 'styled-components';
-import Row from '@paljs/ui/Row';
-import Col from '@paljs/ui/Col';
-import { Button } from '@paljs/ui/Button';
-import React, { useState }  from 'react';
-import Link from 'next/link';
-
-import Layout from 'Layouts';
+import Layout from "Layouts";
 const TableStyle = styled.div`
-display: -webkit-box;
-display: -webkit-flex;
-display: -ms-flexbox;
-display: flex;
--webkit-flex-direction: column;
--ms-flex-direction: column;
-flex-direction: column;
--webkit-flex: 1 1 auto;
--ms-flex: 1 1 auto;
-flex: 1 1 auto;
-overflow: auto;
-width:100%;
-.trasactions_table {
-  width:100%;
-}
-.trasactions_table td,.trasactions_table th{
- padding:15px;
- border-bottom: 1px solid #ccc;
-}
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-flex: 1 1 auto;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  overflow: auto;
+  width: 100%;
+  .trasactions_table {
+    width: 100%;
+  }
+  .trasactions_table td,
+  .trasactions_table th {
+    padding: 15px;
+    border-bottom: 1px solid #ccc;
+  }
 `;
 const PartnerHead = styled.div`
-header{
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: center;
-  padding:10px 20px;
-}
+  header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    padding: 10px 20px;
+  }
 `;
 
 const PartnerCss = styled.div`
-display: flex;
-justify-content: space-between;
-width: 100%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
-const PartHead = styled.div` 
-display: flex;
-justify-content: space-between;
-width: 100%;
-p{
+const PartHead = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  p {
     font-size: 20px;
     font-weight: 600;
-    margin:0;
- }
+    margin: 0;
+  }
 `;
-const Partners = () => {
-  const [, setValue] = useState('');
+const Partners = ({ inventoryList, customers, distributor }) => {
+  const [, setValue] = useState("");
   const submitHandle = (sentValue: string) => setValue(sentValue);
-  const userList = [
-  { name: 'Carla Espinosa',  amount: '2000' },
-    { name: 'Bob Kelso', amount: '4000' },
-    { name: 'Janitor', amount: '5000' },
-    { name: 'Perry Cox', amount: '20000' },
-    { name: 'Ben Sullivan', amount: '21000' },
-  ];
-  const inventoryList = [
-    {
-    name: 'Apple',
-    units:[ 'Kg','Crate','Pieces' ] 
-    },
-    {
-        name: 'Banana',
-        units:[ 'Kg','Crate','Pieces' ] 
-    },
-    {
-        name: 'Orange',
-        units:[ 'Kg','Crate','Pieces' ] 
-     },
-     {
-        name: 'Pinaple',
-        units:[ 'Kg','Crate','Pieces' ] 
-     }
-  ];
 
   const transcList = [
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    { date: '9th Dec', sale: '40000', cash: '35000' },
-    
+    { date: "9th Dec", sale: "40000", cash: "35000" },
+    { date: "9th Dec", sale: "40000", cash: "35000" },
+    { date: "9th Dec", sale: "40000", cash: "35000" },
+    { date: "9th Dec", sale: "40000", cash: "35000" },
+    { date: "9th Dec", sale: "40000", cash: "35000" },
+    { date: "9th Dec", sale: "40000", cash: "35000" },
   ];
   return (
     <Layout title="Partners">
       <Row>
-      <Col breakPoint={{ xs: 12, md: 6 }}>
-        <Card accent="Primary" style={{minHeight:"340px"}}>
-            <CardHeader className="partname_header" style={{ borderBottom: 'none',paddingBottom:0}}>
+        <Col breakPoint={{ xs: 12, md: 6 }}>
+          <Card accent="Primary" style={{ minHeight: "340px" }}>
+            <CardHeader
+              className="partname_header"
+              style={{ borderBottom: "none", paddingBottom: 0 }}
+            >
               <PartHead>
-              <p>Name : Ashish Jindal</p>
-              <p>Mobile : +91-8287563875</p>
+                <p>Name : {distributor.name}</p>
+                <p>Mobile : {distributor.mobileNo}</p>
               </PartHead>
-              
             </CardHeader>
             <CardBody>
-                 <List>
-                 <ListItem>
-                 <PartHead><span><b>Net Outstanind Credit </b></span> <span> RS 15000</span></PartHead>
+              <List>
+                <ListItem>
+                  <PartHead>
+                    <span>
+                      <b>Net Outstanind Credit </b>
+                    </span>{" "}
+                    <span> RS {distributor.outstandingCredit}</span>
+                  </PartHead>
                 </ListItem>
                 <ListItem>
-                <PartHead><span><b>Total Sale </b> </span> <span>Rs 2000000 </span></PartHead>
+                  <PartHead>
+                    <span>
+                      <b>Total Sale </b>{" "}
+                    </span>{" "}
+                    <span>Rs {distributor.totalSale} </span>
+                  </PartHead>
                 </ListItem>
                 <ListItem>
-                <PartHead><span><b>Today Sale </b> </span> <span>Rs 1500</span></PartHead>
+                  <PartHead>
+                    <span>
+                      <b>Total Cash Received</b>{" "}
+                    </span>{" "}
+                    <span>Rs {distributor.totalCashReceived}</span>
+                  </PartHead>
                 </ListItem>
-                 </List>
+              </List>
             </CardBody>
-        </Card>
+          </Card>
         </Col>
         <Col breakPoint={{ xs: 12, md: 6 }}>
-        <Card size="Small" accent="Primary">
-            <CardHeader className="partname_header" style={{fontSize:"20px",textAlign:"center"}}>
-                 inventory List
+          <Card size="Small" accent="Primary">
+            <CardHeader
+              className="partname_header"
+              style={{ fontSize: "20px", textAlign: "center" }}
+            >
+              inventory List
             </CardHeader>
-           <TableStyle>
-            <table className="trasactions_table">
-              {inventoryList.map((invent, index) => (
-              <tr key={index}>
-                <td>{invent.name}</td>
-                <td>{invent.units}</td>
-                <td>
-                <Button appearance="outline" size="Small" status="Primary">
-                    Edit
-                  </Button>
-                </td>
-              </tr>
+            <TableStyle>
+              <table className="trasactions_table">
+                {inventoryList.map((invent, index) => (
+                  <tr key={index}>
+                    <td>{invent.name}</td>
+                    <td>{invent.units}</td>
+                    <td>
+                      <Button
+                        appearance="outline"
+                        size="Small"
+                        status="Primary"
+                      >
+                        Edit
+                      </Button>
+                    </td>
+                  </tr>
                 ))}
               </table>
-              </TableStyle>
-              <CardFooter style={{ textAlign:"center",}}>
-                 <Button appearance="outline" size="Small" status="Primary">
-                    + Add Items
-                  </Button>
-              </CardFooter>
+            </TableStyle>
+            <CardFooter style={{ textAlign: "center" }}>
+              <Button appearance="outline" size="Small" status="Primary">
+                + Add Items
+              </Button>
+            </CardFooter>
           </Card>
-          
         </Col>
-       
       </Row>
       <Row>
-      <Col breakPoint={{ xs: 12, md: 6 }}>
+        <Col breakPoint={{ xs: 12, md: 6 }}>
           <Card size="Small" accent="Primary">
             <PartnerHead>
-            <header>
-              <p style={{fontSize:"18px"}}><b>Customers</b> </p>
-              <Button appearance="outline" size="Small" status="Primary">
-                    + Add Customer
-                  </Button>
-              <div style={{float:"right"}}>
-              <Search submit={(v) => submitHandle(v)} type={'column-curtain'} placeholder="Search..." hint="Hit Enter to search" />
-              </div>
-                
-            </header>
+              <header>
+                <p style={{ fontSize: "18px" }}>
+                  <b>Customers</b>{" "}
+                </p>
+                <Button appearance="outline" size="Small" status="Primary">
+                  + Add Customer
+                </Button>
+                <div style={{ float: "right" }}>
+                  <Search
+                    submit={(v) => submitHandle(v)}
+                    type={"column-curtain"}
+                    placeholder="Search..."
+                    hint="Hit Enter to search"
+                  />
+                </div>
+              </header>
             </PartnerHead>
             <List>
-                  <ListItem>
-                  <PartnerCss>
-                  <p  style={{margin:0}}><b>Name</b></p>
-                  <p className="usramount"  style={{margin:0}}><b>Credit</b></p>
-                  </PartnerCss> 
-                </ListItem>
-              {userList.map((user, index) => (
+              <ListItem>
+                <PartnerCss>
+                  <p style={{ margin: 0 }}>
+                    <b>Name</b>
+                  </p>
+                  <p className="usramount" style={{ margin: 0 }}>
+                    <b>Credit</b>
+                  </p>
+                </PartnerCss>
+              </ListItem>
+              {customers.map((user, index) => (
                 <Link href="/customer-detail">
-                <ListItem key={index} className="partnertile">
-                  <PartnerCss>
-                  <User name={user.name} />
-                  <div className="usramount">(Rs {user.amount})</div>
-                  </PartnerCss> 
-                </ListItem>
+                  <ListItem key={index} className="partnertile">
+                    <PartnerCss>
+                      <User name={user.name} />
+                      <div className="usramount">(Rs {user.creditDue})</div>
+                    </PartnerCss>
+                  </ListItem>
                 </Link>
               ))}
             </List>
@@ -189,31 +195,65 @@ const Partners = () => {
         </Col>
         <Col breakPoint={{ xs: 12, md: 6 }}>
           <Card size="Small" accent="Primary">
-            <header style={{textAlign:"center",fontSize:'18px'}}>Day wise Data</header>
-           <TableStyle>
-            <table className="trasactions_table" style={{textAlign:"center"}}>
-              <tr>
-                <th>Date</th>
-                <th>Sale</th>
-                <th>Cash Received</th>
-              </tr>
-              {transcList.map((transc, index) => (
-                <Link href="/daywise-data"> 
-                <tr key={index} className="partnertile">
-                <td>{transc.date}</td>
-                <td>{transc.sale}</td>
-                <td>{transc.cash}</td>
-              </tr>
-              </Link>
+            <header style={{ textAlign: "center", fontSize: "18px" }}>
+              Day wise Data
+            </header>
+            <TableStyle>
+              <table
+                className="trasactions_table"
+                style={{ textAlign: "center" }}
+              >
+                <tr>
+                  <th>Date</th>
+                  <th>Sale</th>
+                  <th>Cash Received</th>
+                </tr>
+                {transcList.map((transc, index) => (
+                  <Link href="/daywise-data">
+                    <tr key={index} className="partnertile">
+                      <td>{transc.date}</td>
+                      <td>{transc.sale}</td>
+                      <td>{transc.cash}</td>
+                    </tr>
+                  </Link>
                 ))}
               </table>
-              </TableStyle>
+            </TableStyle>
           </Card>
         </Col>
-       
       </Row>
     </Layout>
- 
   );
 };
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(
+    "http://3.7.70.121:8080/inventory/items?distributorId=1"
+  );
+  const inventoryList = await res.json();
+
+  const customersRes = await fetch(
+    "http://3.7.70.121:8080/distributor/list/customer?distributorId=1"
+  );
+  const customers = await customersRes.json();
+
+  const distributorRes = await fetch(
+    "http://3.7.70.121:8080/distributor/id?distributorId=1"
+  );
+  const distributor = await distributorRes.json();
+
+  return {
+    props: {
+      inventoryList,
+      customers,
+      distributor,
+    },
+  };
+}
+
 export default Partners;
